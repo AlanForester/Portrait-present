9c84cfb2e6eb72dcadb46b630547fb4d29825504
Alex Collin <alex@collin.su>
2014-01-13 01:09:32 +0400

    BP

--- frontend/views/test/index.php
+++
@@ -1,9 +0,0 @@
-<?php $this->widget(
-    'yiiwheels.widgets.rangeslider.WhRangeSlider',
-    array(
-        'id'       => 'rangeslidertest',
-        'name'     => 'rangeslidertest',
-        'delayOut' => 4000,
-        'type'     => 'editRange'
-    )
-);?>
